f8918915b19acc61156321d7b7351d824516bb85
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-14 14:44:09 +0600

    add: chat history

--- src/app/all/[id]/page.jsx
+++ src/app/all/[id]/page.jsx
@@ -1,13 +1,39 @@
 'use client'
+import Image from 'next/image';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IoIosArrowRoundBack } from "react-icons/io";
+
 
 const ChatBox = () => {
     const {id} = useParams()
-    console.log(id);
+    const [chat,setChat] = useState([])
+
+    useEffect(()=>{
+        fetch(`https://devapi.beyondchats.com/api/get_chat_messages?chat_id=${id}`)
+        .then(res => res.json())
+        .then(data => setChat(data.data))
+    },[id])
+
     return (
         <div>
-            <h1>hello from chat box</h1>
+            <div>
+                <IoIosArrowRoundBack/>
+                <div>
+                    <Image
+                    src='/assets/user1.jpg'
+                    width={60}
+                    height={60}
+                    alt=''
+                    className='rounded-full'
+                    />
+                   <div>
+                   <h3 className='text-xl'>Shone Jogi</h3>
+                   <p>{chat?.length} members,10 online</p>
+                   </div>
+                </div>
+
+            </div>
         </div>
     );
 };
