031c9cd85b757a353be68db0f344439bb092847f
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-14 19:11:31 +0600

    adding toggling mode

---
+++ src/app/regular/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegularChat = () => {
+    return (
+        <div>
+            <h1>No Message</h1>
+            <button>Add to Chat List</button>
+        </div>
+    );
+};
+
+export default RegularChat;
